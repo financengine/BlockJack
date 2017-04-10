aa09cd5f299b1d3f06240448fe2a8e076f995c78
Tony Cheng <tonycheng@Tonys-MacBook-Pro.local>
2017-04-09 23:22:17 -0700

    first commit

---
+++ app/components/App.js
@@ -0,0 +1,20 @@
+var React = require('react');
+var ReactDOM = require('react-dom');
+
+var App = React.createClass({
+  getInitialState: function() {
+    return {title: 'Best App'};
+  },
+  render: function () {
+    return (
+      <h1>
+        {this.state.title}
+      </h1>
+    );
+  }
+});
+
+ReactDOM.render(<App />,
+               document.getElementById('app'));
+
+module.exports = "app";
---
+++ webpack.config.js
@@ -0,0 +1,32 @@
+var HTMLWebpackPlugin = require('html-webpack-plugin');
+var HTMLWebpackPluginConfig = new HTMLWebpackPlugin({
+  template: __dirname + '/app/index.html',
+  filename: 'index.html',
+  inject: 'body'
+
+});
+module.exports = {
+  entry: __dirname + '/app/index.js',
+  module: {
+    loaders: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        loader: 'babel-loader'
+      }
+    ]
+  },
+  output: {
+    filename: 'transformed.js',
+    path: __dirname + '/build'
+  },
+  plugins: [HTMLWebpackPluginConfig]
+};
+var React = require('react');
+var ReactDOM = require('react-dom');
+var App = require('./components/App');
+
+ReactDOM.render(
+  <App />,
+  document.getElementById('app')
+);
