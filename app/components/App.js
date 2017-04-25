fe9710589665bd6e30c854564d864d6fb3dad3b0
Tony Cheng <tonycheng@airbears2-10-142-129-145.airbears2.1918.berkeley.edu>
2017-04-24 20:20:21 -0700

    Fixed bugs in rendering

--- app/components/App.js
+++ app/components/App.js
@@ -8,7 +8,7 @@ const ETHEREUM_CLIENT = new Web3(new Web3.providers.HttpProvider("http://localho
 
 var smartContractABI = [{"constant":false,"inputs":[],"name":"getBalance","outputs":[{"name":"","type":"uint256"}],"payable":false,"type":"function"},{"constant":true,"inputs":[],"name":"time","outputs":[{"name":"","type":"uint256"}],"payable":false,"type":"function"},{"constant":true,"inputs":[{"name":"","type":"uint256"}],"name":"cardsDrawn","outputs":[{"name":"","type":"uint256"}],"payable":false,"type":"function"},{"constant":false,"inputs":[],"name":"determineWinner","outputs":[],"payable":false,"type":"function"},{"constant":false,"inputs":[],"name":"getPot","outputs":[{"name":"","type":"uint256"}],"payable":false,"type":"function"},{"constant":true,"inputs":[],"name":"pot","outputs":[{"name":"","type":"uint256"}],"payable":false,"type":"function"},{"constant":true,"inputs":[],"name":"buyIn","outputs":[{"name":"","type":"uint256"}],"payable":false,"type":"function"},{"constant":false,"inputs":[],"name":"getNumPlayers","outputs":[{"name":"","type":"uint256"}],"payable":false,"type":"function"},{"constant":false,"inputs":[],"name":"getFirstCard","outputs":[{"name":"","type":"uint256"}],"payable":false,"type":"function"},{"constant":false,"inputs":[{"name":"randSeed","type":"uint256"}],"name":"ante","outputs":[],"payable":false,"type":"function"},{"constant":true,"inputs":[],"name":"numPlayers","outputs":[{"name":"","type":"uint256"}],"payable":false,"type":"function"},{"constant":false,"inputs":[],"name":"addFunds","outputs":[],"payable":true,"type":"function"},{"constant":false,"inputs":[],"name":"getScore","outputs":[{"name":"","type":"uint256"}],"payable":false,"type":"function"},{"constant":true,"inputs":[],"name":"stage","outputs":[{"name":"","type":"uint8"}],"payable":false,"type":"function"},{"constant":false,"inputs":[],"name":"getAllCards","outputs":[{"name":"","type":"uint256[]"}],"payable":false,"type":"function"},{"constant":false,"inputs":[],"name":"dealCards","outputs":[],"payable":false,"type":"function"},{"constant":false,"inputs":[{"name":"addr","type":"address"}],"name":"cashOut","outputs":[{"name":"","type":"bool"}],"payable":false,"type":"function"},{"constant":false,"inputs":[{"name":"hit","type":"bool"},{"name":"randSeed","type":"uint256"}],"name":"play","outputs":[],"payable":false,"type":"function"},{"constant":false,"inputs":[{"name":"randSeed","type":"uint256"}],"name":"addPlayer","outputs":[],"payable":true,"type":"function"},{"constant":false,"inputs":[{"name":"addr","type":"address"}],"name":"getCards","outputs":[{"name":"","type":"uint256[]"}],"payable":false,"type":"function"},{"constant":true,"inputs":[],"name":"test","outputs":[{"name":"","type":"uint256"}],"payable":false,"type":"function"},{"constant":false,"inputs":[],"name":"getBuyIn","outputs":[{"name":"","type":"uint256"}],"payable":false,"type":"function"},{"inputs":[{"name":"randSeed","type":"uint256"},{"name":"playerCap","type":"uint256"},{"name":"bet","type":"uint256"}],"payable":false,"type":"constructor"}]
 
-var smartContractAddress = '0x1e35982f5d8b48931d39b81d1caecc9537f666c5';
+var smartContractAddress = '0xd330cfa12bfa23beca474cac6ad948fad368abc3';
 const smartContract = ETHEREUM_CLIENT.eth.contract(smartContractABI).at(smartContractAddress);
 
 var NavBar = React.createClass({
@@ -33,18 +33,12 @@ var App = React.createClass({
     }
   },
 
-  test: function() {
-    var e = smartContract.test()
-    this.setState({x: e})
-  })
-
   render: function () {
     return (
       <div>
 
-        <NavBar />
-        {test()}
-        <h1 style = {{textAlign: 'center', color: 'white'}}>{thisstate.title}</h1>
+        <NavBar /> 
+        <h1 style = {{textAlign: 'center', color: 'white'}}>{this.state.title}</h1>
         <Blackjackbutton />
         <Pokerbutton />
         <NameForm />
