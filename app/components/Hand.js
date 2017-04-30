43eddf50eb6ab5d959a2c1eca6f311bbeb9475d4
Dylan Kiet Duong <Dyduong@calvisitor-10-105-175-15.calvisitor.1918.berkeley.edu>
2017-04-29 17:59:00 -0700

    Card Pictures

--- app/components/Hand.js
+++ app/components/Hand.js
@@ -17,8 +17,7 @@ var Hand = React.createClass({
   render: function () {
     return (
       <div style = {{color: 'white'}}>
-        <h1>{this.props.player}: Card One - {this.props.info[0]} of {this.props.info[1]} | Card Two - {this.props.info[2]} of {this.props.info[3]}</h1>
-        <h2><img src={'./app/component/cards/'+this.props.info[0]+'_'+this.props.info[1]+'.png'} /></h2>
+        <h1>{this.props.player}: Card One - {this.props.info[0]} of {this.props.info[1]} <img src={'./cards/'+ this.props.info[0] +'_of_'+ this.props.info[1] +'.png'} style={{width: 50, height: 50}} />| Card Two - {this.props.info[2]} of {this.props.info[3]}<img src={'./cards/'+ this.props.info[0] +'_of_'+ this.props.info[1] +'.png'} style={{width: 50, height: 50}} /></h1>
       </div>
       )
     }
